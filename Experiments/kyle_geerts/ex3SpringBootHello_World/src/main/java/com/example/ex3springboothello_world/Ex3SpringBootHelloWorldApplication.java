bb5d151431c0956a354c3df10db262b6d6512d62
KyleGeerts <kgeerts@iastate.edu>
2022-02-05 10:40:24 -0600

    Setting up for a better webpage

---
+++ Experiments/kyle_geerts/ex3SpringBootHello_World/src/main/java/com/example/ex3springboothello_world/Ex3SpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex3springboothello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex3SpringBootHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex3SpringBootHelloWorldApplication.class, args);
+    }
+
+}
