17bcb664d5b8f7269ffaa4a5c84cc86eb6c356e0
Kyle_Goben <kgoben@iastate.edu>
2022-02-05 11:15:05 -0600

    New post mapping tester

---
+++ Experiments/kyle_goben/exp3_new-world/src/test/java/com/example/exp3_newworld/Exp3NewWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exp3_newworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp3NewWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
