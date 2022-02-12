e0b933755b9bbad040fe005426e80ebce054c5bc
Kyle_Goben <kgoben@iastate.edu>
2022-02-12 10:00:14 -0600

    People and user controller

--- Experiments/kyle_goben/exp3_new-world/src/main/java/com/example/exp3_newworld/new_world_controller.java
+++ Experiments/kyle_goben/exp3_new-world/src/main/java/com/example/exp3_newworld/new_world_controller.java
@@ -11,22 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class new_world_controller {
+    TestData td;
 
     @GetMapping("/getTest")
     public String getTest(@RequestParam(value = "username", defaultValue = "World") String message) {
-        return String.format("Hello, %s! You sent a get request with a parameter!", message);
+        return String.format("Hello, %s! You sent a get request with a parameter!", td.getName());
     }
 
     @PostMapping("/postTest1")
     public String postTest1(@RequestParam(value = "username", defaultValue = "World") String message) {
-
-        return String.format("Hello, %s! You sent a post request with a parameter!", message);
+        td = new TestData();
+        return String.format("Hello, %s! You sent a post request with a parameter!", td.getName());
     }
+
+
 
     @PostMapping("/postTest2")
     public String postTest2(@RequestBody TestData testData) {
         return String.format("Hello, %s! Your favorite color is %s. Your password is %s. Your phone number is %s.", testData.getName(),
-                testData.getFaveColor(),testData.getPassword(),testData.getPhoneNum());
+                testData.getFaveColor(), testData.getPassword(), testData.getPhoneNum());
     }
 
     @DeleteMapping("/deleteTest")
