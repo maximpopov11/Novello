60a4beff4246ae355ce4a987872172cb4d745ea0
Maxim Popov <maximpopov@mpopov-mac.student.iastate.edu>
2022-02-16 15:24:36 -0600

    Allowed user to input string for book title to later be searched for in database (that part has not yet been made). Currently has a bug where user input is not saved correctly and thus crashes when attempts to use data that ends up being null.

--- Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/Activities/MainActivity.java
+++ Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/Activities/MainActivity.java
@@ -3,14 +3,36 @@ package com.yn_1.demo2_volleyproject.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.yn_1.demo2_volleyproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView searchBar;
+    EditText input;
+    String searched;
+    Button submitSearchButton;
+    TextView showSearched;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBar = findViewById(R.id.searchBookTitle);
+        searchBar.setHint("Enter book title...");
+        submitSearchButton = findViewById(R.id.submitSearch);
+        submitSearchButton.setText("Submit");
+        submitSearchButton.setOnClickListener(v -> {
+            //todo: crashes on next line because input is not set to what the user inputted
+            searched = input.getText().toString();
+            showSearched.setText(input.getText());
+        });
+        showSearched = findViewById(R.id.showSearched);
+
     }
+
 }
