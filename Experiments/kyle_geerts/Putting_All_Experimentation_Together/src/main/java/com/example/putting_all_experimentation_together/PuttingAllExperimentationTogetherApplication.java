ba1629a7a55573d58129823c2133c5f77667899a
KyleGeerts <kgeerts@iastate.edu>
2022-02-19 11:27:25 -0600

    Using h2 for the first time!

---
+++ Experiments/kyle_geerts/Putting_All_Experimentation_Together/src/main/java/com/example/putting_all_experimentation_together/PuttingAllExperimentationTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.putting_all_experimentation_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuttingAllExperimentationTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuttingAllExperimentationTogetherApplication.class, args);
+    }
+
+}
