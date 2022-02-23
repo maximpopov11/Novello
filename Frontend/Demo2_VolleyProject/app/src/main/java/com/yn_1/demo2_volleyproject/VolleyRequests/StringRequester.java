a7b4092c44d4d3e12e47cec979d053acc0da51c8
Maxim Popov <maximpopov@mpopov-mac.student.iastate.edu>
2022-02-23 15:29:06 -0600

    Finished all methods in StringRequester except for all of them have the same three compile-time errors as getRequest did when I commited that earlier today.

--- Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/VolleyRequests/StringRequester.java
+++ Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/VolleyRequests/StringRequester.java
@@ -8,6 +8,7 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Header;
 import com.android.volley.Request;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.yn_1.demo2_volleyproject.AppController;
 import com.yn_1.demo2_volleyproject.VolleyCommand;
@@ -63,19 +64,97 @@ public class StringRequester implements Requester<String> {
     @Override
     public void postRequest(String url, String post, VolleyCommand command,
                             Map<String, String> headers, Map<String, String> params) {
+        StringRequest postStringRequest = new StringRequest(
+                Request.Method.POST, url, post,
+                response -> {
+                    Log.d(TAG, response.toString());
+                    command.execute(response);
+                }, error -> {
+            VolleyLog.d(TAG, "Error: " + error.getMessage());
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null)
+                    return super.getHeaders();
+                else
+                    return headers;
+            }
 
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if (headers == null)
+                    return super.getParams();
+                else
+                    return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(postStringRequest, TAG);
     }
 
     @Override
     public void putRequest(String url, String put, VolleyCommand command,
                            Map<String, String> headers, Map<String, String> params) {
+        StringRequest putStringRequest = new StringRequest(
+                Request.Method.PUT, url, put,
+                response -> {
+                    Log.d(TAG, response.toString());
+                    command.execute(response);
+                }, error -> {
+            VolleyLog.d(TAG, "Error: " + error.getMessage());
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null)
+                    return super.getHeaders();
+                else
+                    return headers;
+            }
 
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if (headers == null)
+                    return super.getParams();
+                else
+                    return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(putStringRequest, TAG);
     }
 
     @Override
     public void deleteRequest(String url, VolleyCommand command,
                               Map<String, String> headers, Map<String, String> params) {
+        StringRequest deleteStringRequest = new StringRequest(
+                Request.Method.DELETE, url, null,
+                response -> {
+                    Log.d(TAG, response.toString());
+                    command.execute(response);
+                }, error -> {
+            VolleyLog.d(TAG, "Error: " + error.getMessage());
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                if (headers == null)
+                    return super.getHeaders();
+                else
+                    return headers;
+            }
 
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if (headers == null)
+                    return super.getParams();
+                else
+                    return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(deleteStringRequest, TAG);
     }
 
 }
