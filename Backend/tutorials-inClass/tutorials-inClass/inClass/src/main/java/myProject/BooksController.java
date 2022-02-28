b411713bc7abb360b5346c3d4346088460ddc266
KyleGeerts <kgeerts@iastate.edu>
2022-02-28 08:56:14 -0600

    To be able to be checked out for front end

---
+++ Backend/tutorials-inClass/tutorials-inClass/inClass/src/main/java/myProject/BooksController.java
@@ -0,0 +1,39 @@
+package myProject;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BooksController {
+
+    @Autowired
+    BooksDB db;
+
+    @PostMapping("/addBooks")
+    Books creatBooks(@RequestBody Books b)
+    {
+        db.save(b);
+        return b;
+    }
+
+//    @GetMapping("/book/{title}")
+//    Books getBook(@PathVariable String title){
+//        return db.findById().equals(title);//how to find a column
+//
+//    }
+
+
+    @RequestMapping("/books")
+    List<Books> showMeTheBooks() {
+        return db.findAll();
+    }
+
+
+    @DeleteMapping("/book/{id}")
+    String deleteBook(@PathVariable Integer id) {
+        db.deleteById(id);
+        return "deleted " + id;
+    }
+}
