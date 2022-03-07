81dd1c006f442118ec01cb9a32c5aa505e46c0f4
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2022-03-07 16:32:28 -0600

    Deleted duplicate android project, fixed front-end pipelining

--- Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/Const.java
+++
@@ -1,6 +0,0 @@
-package com.yn_1.demo2_volleyproject;
-
-public class Const {
-    public final static String baseUrl="http://coms-309-013.class.las.iastate.edu:8080/";
-    public final static String postmanMockUrl="https://06817084-8904-405a-8e1b-2c60a3fae2a2.mock.pstmn.io/";
-}
--- Frontend/Demo2_VolleyProject/app/src/main/java/com/yn_1/demo2_volleyproject/account/UserType.java
+++
@@ -1,5 +0,0 @@
-package com.yn_1.demo2_volleyproject.account;
-
-public enum UserType {
-    ADMIN, MODERATOR, ADULT, CHILD
-}
