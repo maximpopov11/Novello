b15cbd8dcb6dd2f4bb96d799eafe3954d28c542d
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2022-03-17 21:53:16 -0500

    Navigation controller finally working!

--- Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/NavBarActivity.java
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/NavBarActivity.java
@@ -3,17 +3,20 @@ package com.yn_1.novello_app;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.NavHost;
+import androidx.navigation.NavHostController;
 import androidx.navigation.ui.NavigationUI;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.yn_1.novello_app.home.HomeFragment;
+import com.yn_1.novello_app.library.LibraryFragment;
 
 public class NavBarActivity extends AppCompatActivity {
 
@@ -22,31 +25,29 @@ public class NavBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_bar);
 
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        BottomNavigationView navBar = findViewById(R.id.bottom_nav);
 
-        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
-                switch (destination.getId())
-                {
-                    case R.id.homeFragment:
-                        bottomNav.setVisibility(View.VISIBLE);
-                        navController.navigate(R.id.homeFragment);
-                        break;
-                    case R.id.libraryFragment:
-                        bottomNav.setVisibility(View.VISIBLE);
-                        navController.navigate(R.id.libraryFragment);
-                        break;
-                    default:
-                        bottomNav.setVisibility(View.GONE);
-                        Log.d("Navigation", "Button received error");
-                        break;
+        ((NavigationBarView) navBar).setOnItemSelectedListener (
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                    switch (item.getItemId()) {
+                        case R.id.homeFragment:
+                            transaction.replace(R.id.nav_host_fragment, new HomeFragment()).commit();
+                            return true;
+
+                        case R.id.libraryFragment:
+                            transaction.replace(R.id.nav_host_fragment, new LibraryFragment()).commit();
+                            return true;
+
+                        default:
+                            return false;
+                    }
                 }
             }
-        });
+        );
     }
 }
