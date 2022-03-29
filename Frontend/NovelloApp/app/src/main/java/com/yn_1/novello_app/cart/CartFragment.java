67d123c3ff2513a9471a7a257334bb02b66b644e
maximpopov <icecreamguy0@gmail.com>
2022-03-29 12:09:07 -0500

    Cart fragment created.

---
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/cart/CartFragment.java
@@ -0,0 +1,17 @@
+package com.yn_1.novello_app.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class CartFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        //todo: show books in cart
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
