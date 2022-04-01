260703c142a2c73e3c9eb1f33d3254dbe95cc9c6
maximpopov <icecreamguy0@gmail.com>
2022-04-01 10:31:41 -0500

    Adds book image view to cart screen. Likely does so incorrectly, currently unable to test.

--- Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/cart/CartView.java
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/cart/CartView.java
@@ -1,13 +1,16 @@
 package com.yn_1.novello_app.cart;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
+import com.yn_1.novello_app.Const;
 import com.yn_1.novello_app.NavBarActivity;
 import com.yn_1.novello_app.R;
 import com.yn_1.novello_app.account.User;
@@ -28,6 +31,9 @@ public class CartView extends Fragment {
 
     Button purchase;
     LinearLayout innerLayout;
+
+    final int Image_Width = 175;
+    final int Image_Height = 280;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,10 +68,22 @@ public class CartView extends Fragment {
      */
     private void addBooksToLayout(List<Book> cartBooks) {
         for (Book book : cartBooks) {
+            //todo: show book info in rows: imageButton, title, author, price, remove from cart button, move to wishlist
             ImageButton imageButton = new ImageButton(getView().getContext());
-            //todo: show book info in rows: image, title, author, price, remove from cart button, move to wishlist
-            //innerLayout.addView(book.getImage());
-            //todo: show book info (price, author, etc.) next to image
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(Image_Width, Image_Height);
+            params.setMargins(15, 0, 15, 0);
+            imageButton.setLayoutParams(params);
+            imageButton.setBackgroundColor(Color.YELLOW);
+            //todo: set image bitmap
+
+            imageButton.setOnClickListener(v -> {
+                //todo: image button on click
+            });
+
+            book.setImageButton(imageButton);
+
+            innerLayout.addView(book.getImageButton());
         }
     }
 
