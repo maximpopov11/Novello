397a3314fdae3576f20a3b5a396c55f18f8b8da5
KyleGeerts <kgeerts@iastate.edu>
2022-04-03 12:20:51 -0500

    Review completed you can make a review and a rating.

--- Backend/tutorials-inClass/tutorials-inClass/inClass/src/main/java/myProject/BookRatingController.java
+++ Backend/tutorials-inClass/tutorials-inClass/inClass/src/main/java/myProject/BookRatingController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
-import java.util.List;
-import java.util.Optional;
+
 
 
 @RestController
@@ -17,6 +15,7 @@ public class BookRatingController {
     BooksDB bdb;
     @Autowired
     PersonDB pdb;
+
     @PostMapping("/addRating/{bid}/{pid}")
     BookRating creatBookRating(@PathVariable Integer bid, @PathVariable Integer pid, @RequestBody BookRating br) {
         //br.setBook(bdb.findById(bid).orElseThrow());
@@ -35,55 +34,32 @@ public class BookRatingController {
         db.save(br);
         return br;
     }
-   @GetMapping("/getRating/{bid}/{pid}")
-   BookRating  findAReview(@PathVariable Integer bid, @PathVariable Integer pid)
-   {
-       BookRatingKey brk = new BookRatingKey();
-       brk.setBookId(bid);
-       brk.setPersonId(pid);
-       BookRating ratingToReturn = db.findById(brk).orElseThrow();
-       return ratingToReturn;
-   }
-   @PutMapping("/addReview/{bid}/{pid}")
+
+    @GetMapping("/getRating/{bid}/{pid}")
+    BookRating findAReview(@PathVariable Integer bid, @PathVariable Integer pid) {
+        BookRatingKey brk = new BookRatingKey();
+        brk.setBookId(bid);
+        brk.setPersonId(pid);
+        BookRating ratingToReturn = db.findById(brk).orElseThrow();
+        return ratingToReturn;
+    }
+
+    @PutMapping("/addReview/{bid}/{pid}")
     BookRating creatReview(@PathVariable Integer bid, @PathVariable Integer pid, @RequestBody BookRating br) {
-       BookRatingKey brk = new BookRatingKey();
-       brk.setBookId(bid);
-       brk.setPersonId(pid);
-       BookRating oldRating = db.findById(brk).orElseThrow();
-       if(oldRating.getRating() !=0)
-       {
-           br.setRating(oldRating.getRating());
-       }
-       Person p = pdb.findById(pid).orElseThrow();
-       Books b = bdb.findById(bid).orElseThrow();
-       br.setId(brk);
-       br.setBook(b);
-       br.setPerson(p);
-       db.save(br);
+        BookRatingKey brk = new BookRatingKey();
+        brk.setBookId(bid);
+        brk.setPersonId(pid);
+        BookRating oldRating = db.findById(brk).orElseThrow();
+        if (oldRating.getRating() != 0) {
+            br.setRating(oldRating.getRating());
+        }
+        Person p = pdb.findById(pid).orElseThrow();
+        Books b = bdb.findById(bid).orElseThrow();
+        br.setId(brk);
+        br.setBook(b);
+        br.setPerson(p);
+        db.save(br);
 
-       return br;
-   }
-//        //br.setBook(bdb.findById(bid).orElseThrow());
-//        //br.setPerson(pdb.findById(pid).orElseThrow());
-//
-//        BookRatingKey brk = new BookRatingKey();
-//        brk.setBookId(bid);
-//        brk.setPersonId(pid);
-//
-//       //BookRating oldBR = db.findById(brk);
-//       //Optional<BookRating> oldBR = frdb.findById(brk);
-//
-//
-//
-//        br.setId(brk);
-//
-//        db.save(br);
-//        return br;
-//    }
-
-//    @GetMapping("/book/{bid}/person/{pid}")
-//    Optional<BookRating> getBookRating(@PathVariable Integer bid, @PathVariable Integer pid) {
-//        return db.findById(pid);
-//
-//    }
+        return br;
+    }
 }
