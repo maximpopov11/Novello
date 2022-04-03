a6f85edac5823d83d2a8111456518a084cd084b6
maximpopov11 <icecreamguy0@gmail.com>
2022-04-03 16:20:14 -0500

    sendBooksToPresenter almost completed. Does not yet compile. Battery is almost out...

--- Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/discovery/DiscoveryModel.java
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/discovery/DiscoveryModel.java
@@ -1,10 +1,16 @@
 package com.yn_1.novello_app.discovery;
 
 import com.android.volley.VolleyError;
+import com.yn_1.novello_app.book.Book;
 import com.yn_1.novello_app.volley_requests.JsonArrayRequester;
 import com.yn_1.novello_app.volley_requests.VolleyCommand;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Model for the discovery screen
@@ -38,6 +44,26 @@ public class DiscoveryModel {
      * @param data
      */
     private void sendBooksToPresenter(JSONArray data) {
+        ArrayList<Book> allBooks = new ArrayList<>();
+        for (int i = 0; i < data.length(); i++) {
+            try {
+                JSONObject book = data.getJSONObject(i);
+                int bookID = book.getInt("bookID");
+                String title = book.getString("title");
+                String author = book.getString("author");
+                int publicationYear = book.getInt("publicationYear");
+                String isbn = book.getString("isbn");
+                int rating = book.getInt("rating");
+                String imageUrl = book.getString("imageUrl");
+                Book newBook = new Book(bookID, title, author, publicationYear, isbn, rating, -1, null, imageUrl);
+                newBook.setUserCategoryID(categoryPath);
+                bookCollection.add(newBook);
+                bookCount++;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            presenter.sendAllBooks(allBooks);
+        }
         //todo: send books to presenter
     }
 
