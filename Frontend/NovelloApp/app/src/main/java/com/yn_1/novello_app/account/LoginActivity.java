3ded2c17b29d263fde0e8644a14da7b1fcc7420f
maximpopov11 <icecreamguy0@gmail.com>
2022-04-06 12:22:08 -0500

    UserLogin accepting null json as failed input in addition to 0 user id as failed.

--- Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/account/LoginActivity.java
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/account/LoginActivity.java
@@ -93,7 +93,13 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public void execute(JSONObject data) {
             try {
-                int userID = data.getInt("userID");
+                int userID;
+                if (data != null) {
+                    userID = data.getInt("userID");
+                }
+                else {
+                    userID = 0;
+                }
                 loginResult(userID);
             } catch (JSONException e) {
                 e.printStackTrace();
