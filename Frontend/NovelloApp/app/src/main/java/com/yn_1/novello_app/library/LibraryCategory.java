329ee321199adb3e1531c03675f8931448a3581a
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2022-04-05 19:03:50 -0500

    Library category enumerated type. Performs category transactions.

---
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/library/LibraryCategory.java
@@ -0,0 +1,105 @@
+package com.yn_1.novello_app.library;
+
+import android.util.Log;
+
+import com.android.volley.VolleyError;
+import com.yn_1.novello_app.volley_requests.JsonObjectRequester;
+import com.yn_1.novello_app.volley_requests.VolleyCommand;
+
+import org.json.JSONObject;
+
+public enum LibraryCategory {
+    NONE (0, null, new boolean[]{false, true, true, false, false, false}),
+    WISHLIST (1, "wishlist", new boolean[]{true, false, true, false, false, false}),
+    CART (2, "cart", new boolean[]{true, true, false, true, false, false}),
+    BACKLOG (3, "backlog", new boolean[]{false, false, false, false, true, false}),
+    CURRENTLY_READING (4, "currently_reading", new boolean[]{false, false, false, true, false, true}),
+    READ (5, "read", new boolean[]{false, false, false, true, true, false});
+
+    private int categoryIndex;
+    private final String stringFormat;
+    private final boolean[] possibleTransactions;
+
+    LibraryCategory(int categoryIndex, String stringFormat, boolean[] possibleTransactions) {
+        this.categoryIndex = categoryIndex;
+        this.stringFormat = stringFormat;
+        this.possibleTransactions = possibleTransactions;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getCategoryIndex() {
+        return categoryIndex;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getStringFormat() {
+        return stringFormat;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean[] getPossibleTransactions() {
+        return possibleTransactions;
+    }
+
+    /**
+     * Method for moving book between user categories.
+     * @param newCategory
+     * @param userID
+     * @param bookID
+     */
+    public void performTransaction(LibraryCategory newCategory, int userID, int bookID) {
+
+        // Check if current category can move to new category
+        if (possibleTransactions[newCategory.getCategoryIndex()]) {
+            // Get the book.
+            JsonObjectRequester bookReq = new JsonObjectRequester();
+            bookReq.getRequest("books/" + bookID, null, new VolleyCommand<JSONObject>() {
+                @Override
+                public void execute(JSONObject book) {
+                    // Call a helper method to move the book to a new category.
+                    putInCategory(newCategory, userID, book);
+                }
+
+                @Override
+                public void onError(VolleyError error) {
+                    Log.d("Transaction", "Could not obtain Book at book ID " + bookID);
+                }
+            }, null, null);
+        }
+        else
+        {
+            Log.d("Transaction", "Invalid category transaction.");
+        }
+    }
+
+    /**
+     * Helper method for putting book in a new category
+     * @param newCategory
+     * @param userID
+     * @param book
+     */
+    private void putInCategory(LibraryCategory newCategory, int userID, JSONObject book) {
+        JsonObjectRequester categoryReq = new JsonObjectRequester();
+        String pathUrl = "library/" + userID + "/" + newCategory.getCategoryIndex();
+        categoryReq.putRequest(pathUrl, book, new VolleyCommand<JSONObject>() {
+            @Override
+            public void execute(JSONObject data) {
+                Log.d("Transaction", "Transaction successfully completed.");
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                Log.d("Transaction", "Unable to PUT book in user category.");
+            }
+        }, null, null);
+    }
+}
