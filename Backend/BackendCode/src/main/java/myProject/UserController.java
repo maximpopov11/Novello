8ff73cc3bb24600d0cbcc35cc61d1dbb97f8ac92
KyleGeerts <kgeerts@iastate.edu>
2022-04-13 15:21:28 -0500

    The api for userController

--- Backend/BackendCode/src/main/java/myProject/UserController.java
+++ Backend/BackendCode/src/main/java/myProject/UserController.java
@@ -1,12 +1,13 @@
 package myProject;
 
+import io.swagger.annotations.*;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
-
+@Api(value = "UserController")
 @RestController
 public class UserController {
 
@@ -16,14 +17,28 @@ public class UserController {
     @Autowired
     UserInfoInterface userInfoInterfaceDB;
 
+    @ApiOperation(value = "Get a user from a userID", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping("/user/{id}")
-	User getUser(@PathVariable Integer id) {
+	User getUser(@ApiParam(value = "ID of the user you are looking for") @PathVariable Integer id) {
         return db.findById(id).
                 orElseThrow(RuntimeException::new);
     }
 
+    @ApiOperation(value = "Gets the username if login is successful", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping("/login")
-    JSONObject login(@RequestBody JSONObject json){
+    JSONObject login(@ApiParam (value = "json opject that has the username and password entered by user to check if they can log in")@RequestBody JSONObject json){
 
         User user;
         JSONObject jsonReturn = new JSONObject();
@@ -40,9 +55,15 @@ public class UserController {
         return null;
     }
 
-
+    @ApiOperation(value = "Creates all the users at once and adds them to the data base", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PostMapping("/addAllUsers")
-    void createAllPersons(@RequestBody JSONObject[] jsonObject) {
+    void createAllPersons(@ApiParam (value = "All of the users jsonObjects")@RequestBody JSONObject[] jsonObject) {
         for(int i = 0; i < jsonObject.length; i++)
         {
             User u = new User();
@@ -64,14 +85,27 @@ public class UserController {
             userInfoInterfaceDB.save(ui);
         }
     }
-
+    @ApiOperation(value = "Gets all users", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping("/users")//was request mapping
     List<User> getPersons() {
         return db.findAll();
     }
 
+    @ApiOperation(value = "Creates a specific user", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PostMapping("/user")
-	User createPerson(@RequestBody JSONObject jsonObject) {
+	User createPerson(@ApiParam (value = "jsonObject with all of the info of a user")@RequestBody JSONObject jsonObject) {
         User u = new User();
         u.setAccountType((Integer) jsonObject.getAsNumber("accountType"));
         u.setUsername(jsonObject.getAsString("username"));
@@ -91,9 +125,15 @@ public class UserController {
         userInfoInterfaceDB.save(ui);
         return u;
     }
-
+    @ApiOperation(value = "updating a user via a userID", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PutMapping("/user/{id}")
-	User updatePerson(@RequestBody JSONObject jsonObject, @PathVariable Integer id) {
+	User updatePerson(@ApiParam (value = "The Jason object of the updated person")@RequestBody JSONObject jsonObject, @ApiParam (value = "The ID of the person we are updating")@PathVariable Integer id) {
         User old_u = db.findById(id).orElseThrow(RuntimeException::new);
         UserInfo old_ui = old_u.getUserInfo();
         if (jsonObject.getAsString("name") != null)
@@ -116,9 +156,15 @@ public class UserController {
         userInfoInterfaceDB.save((old_ui));
         return old_u;
     }
-
+    @ApiOperation(value = "Deletes a user from a userID", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @DeleteMapping("/user/{id}")
-    String deletePerson(@PathVariable Integer id) {
+    String deletePerson(@ApiParam (value = "The ID of the user you want to delete")@PathVariable Integer id) {
         db.deleteById(id);
         return "deleted " + id;
     }
