ae36b6c75de51e544784e48852007fa2f057cc4e
maximpopov11 <icecreamguy0@gmail.com>
2022-04-20 14:20:15 -0500

    Get Friends List request implemented.

--- Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/account/ProfileModel.java
+++ Frontend/NovelloApp/app/src/main/java/com/yn_1/novello_app/account/ProfileModel.java
@@ -1,6 +1,13 @@
 package com.yn_1.novello_app.account;
 
+import com.android.volley.VolleyError;
 import com.yn_1.novello_app.cart.CartPresenter;
+import com.yn_1.novello_app.volley_requests.JsonArrayRequester;
+import com.yn_1.novello_app.volley_requests.VolleyCommand;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -33,7 +40,17 @@ public class ProfileModel {
      * Receives the friends list from the database
      */
     public void getFriendsList() {
-        //todo: backend request to get friends
+        JsonArrayRequester jsonArrayRequester = new JsonArrayRequester();
+        JsonArrayCommand command = new JsonArrayCommand();
+        JSONArray userJson = new JSONArray();
+        try {
+            JSONObject jsonElement = new JSONObject();
+            jsonElement.put("id", user.getUserId());
+            userJson.put(jsonElement);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        jsonArrayRequester.getRequest("friends", userJson, command, null, null);
     }
 
     /**
@@ -42,6 +59,23 @@ public class ProfileModel {
      */
     private void setFriendsList(ArrayList<User> friendsList) {
         presenter.setFriendsList(friendsList);
+    }
+
+    /**
+     * Class used to get result of request
+     */
+    private class JsonArrayCommand implements VolleyCommand<JSONArray> {
+
+        @Override
+        public void execute(JSONArray data) {
+            //todo: parse data into ArrayList and call setFriendsList with that as a param
+        }
+
+        @Override
+        public void onError(VolleyError error) {
+
+        }
+
     }
 
 }
