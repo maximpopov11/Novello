b5636126b327b91327984bc912fdd7ecfc2e9552
Kyle_Goben <kgoben@iastate.edu>
2022-04-27 08:50:58 -0500

    Small fix

--- Backend/BackendCode/src/main/java/myProject/chat/ChatRoomController.java
+++ Backend/BackendCode/src/main/java/myProject/chat/ChatRoomController.java
@@ -1,5 +1,7 @@
 package myProject.chat;
 
+import myProject.User;
+import myProject.UserInterface;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +9,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
+import java.util.Set;
+
 @RestController
 public class ChatRoomController {
     @Autowired
     ChatRoomRepository cdb;
+    @Autowired
+    UserInterface udb;
 
-    @PostMapping("/chatRoom")
-    void createRoom(@RequestBody ChatRoom chatRoom){
-        cdb.save(chatRoom);
-    }
+//    @PostMapping("/chatRoom")
+//    void createRoom(@RequestBody JSONObject[] json){
+//        User user;
+//
+//        for(int i = 0; i<json.length; i++){
+//            user = udb.findById((int) json[i].getAsNumber("userId")).orElseThrow(NoSuchElementException::new);
+//        }
+////        cdb.save(chatRoom);
+//    }
+
+
 //    @GetMapping("/chatRoom")
 //    void getRooms(@RequestBody JSONObject json[]){
 //        int user = (int) json[0].getAsNumber("userId");
